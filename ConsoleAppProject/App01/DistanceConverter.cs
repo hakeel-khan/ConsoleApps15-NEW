211273ec3442620cf5c7374b41ae26f6ec98e8e8
hakeel-khan <hakeelkhan5@gmail.com>
2021-02-04 18:38:19 +0000

    Distance cover copy

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// Please describe the main features of this App
@@ -8,6 +10,42 @@
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+        /// <summary>
+        /// Here we will use the Run method to call it in the program class which will allow it to run
+        /// </summary>
+        public void Run()
+        {
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+        /// <summary>
+        /// Here the user will input the Miles
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write("Enter the number of miles > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+        /// <summary>
+        /// In this method it will calculate the feet
+        /// </summary>
+        private void CalculateFeet()
+        {
+            feet = miles * 5280
+        }
+        /// <summary>
+        /// Here when the programmed is running it will output the feet
+        /// </summary>
+        private void OutputFeet()
+        {
+            Console.WriteLine(miles + "miles is" + feet + "Feet!");
+        }
 
     }
 }

91544b99b81d5e6c73ff57472538a10a8dc3ab81
hakeel-khan <hakeelkhan5@gmail.com>
2021-02-04 18:33:04 +0000

    program copy

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppProject.App01;
+using System;
 
 namespace ConsoleAppProject
 {
@@ -8,17 +9,20 @@ namespace ConsoleAppProject
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Hakeel Khan 01/02/2021
     /// </summary>
     public static class Program
     {
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            
+
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
             Console.Beep();
+
+            DistanceConverter converter = new DistanceConverter();
+            converter.Run;
 
 
         }

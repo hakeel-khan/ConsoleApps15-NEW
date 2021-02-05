0c0bdac3f1c472eed5891a8f1ba96ea68e22fdcd
hakeel-khan <hakeelkhan5@gmail.com>
2021-02-05 18:03:59 +0000

    heading update

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -20,18 +20,18 @@ namespace ConsoleAppProject
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
             Console.Beep();
+            Console.WriteLine(GetHeading("Convert Distances"));
 
             DistanceConverter converter = new DistanceConverter();
             converter.Run();
-            Console.WriteLine(GetHeading("Convert Distances"));
 
         }
         private static string GetHeading(string title)
         {
             return "--------------------------------\n" 
-                + "\t\t" + title 
-                + "\n\t\Hakeel Khan" +
-                "--------------------------------\n";
+                + "\t" + title 
+                + "\n\tHakeel Khan" +
+                "\n--------------------------------\n";
         }
     }
 }

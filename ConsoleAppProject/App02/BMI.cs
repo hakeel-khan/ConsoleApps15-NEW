597a90f8ddc7aede871a96ff6629c49053690936
hakeel-khan <hakeelkhan5@gmail.com>
2021-03-02 14:51:05 +0000

    output heading

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,12 +1,31 @@
-﻿namespace ConsoleAppProject.App02
+﻿using System;
+namespace ConsoleAppProject.App02
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// Here in this App it will be a Body Mass Index calculator which will calculate the users height and weight and health risks.
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Student Hakeel Khan version 0.1
     /// </author>
     public class BMI
     {
+            double weightInStones;
+            double weightInPounds;
+            double heightInFeet;
+            double heightInInches;
+            double weightInKg;
+            double heightInMetres;
+
+
+        public void OutputHeading()
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("------Body Mass Index Calculator------");
+            Console.WriteLine("-----------By Hakeel Khan-------------");
+            Console.WriteLine("--------------------------------------");
+
+        }
+
+
     }
 }

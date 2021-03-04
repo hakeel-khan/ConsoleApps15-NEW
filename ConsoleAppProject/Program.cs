5d8facdb3063303e00a74ef37f00e149b043288c
hakeel-khan <hakeelkhan5@gmail.com>
2021-03-04 21:21:49 +0000

    BMI

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -21,8 +21,8 @@ namespace ConsoleAppProject
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
 
-            Console.WriteLine("Please choose from the following >");
-            Console.WriteLine();
+            //Console.WriteLine("Please choose from the following >");
+            //Console.WriteLine();
 
             //DistanceConverter converter = new DistanceConverter();
             //converter.ConvertDistance();

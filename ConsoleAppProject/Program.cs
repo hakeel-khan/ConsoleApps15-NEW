d986e9e250d864c5ca6a961cab3532640dcd2467
hakeel-khan <hakeelkhan5@gmail.com>
2021-03-09 21:32:05 +0000

    user interface

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -13,13 +13,38 @@ namespace ConsoleAppProject
     /// Hakeel Khan 01/02/2021
     /// </summary>
     public static class Program
-    {
+    { 
+        private static DistanceConverter converter = new DistanceConverter();
+        private static BMI calculator = new BMI();
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine("----------------------------------------------");
             Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+            Console.WriteLine("----------------------------------------------");
             Console.WriteLine();
+
+            Console.WriteLine("1. Distance Converter");
+            Console.WriteLine("2. BMI Calculator");
+            Console.WriteLine();
+
+            Console.WriteLine("Please choose which app you would like to choose from > ");
+            string choice = Console.ReadLine();
+
+            if(choice == "1")
+            {
+                converter.ConvertDistance();
+            }
+            else if (choice == "2")
+            {
+                calculator.BMICalculation();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Choice");
+            }
+
 
             //Console.WriteLine("Please choose from the following >");
             //Console.WriteLine();
@@ -27,8 +52,8 @@ namespace ConsoleAppProject
             //DistanceConverter converter = new DistanceConverter();
             //converter.ConvertDistance();
 
-            BMI calculator = new BMI();
-            calculator.BMICalculation();
+            ///BMI calculator = new BMI();
+            ///calculator.BMICalculation();
         }
     }
 }
